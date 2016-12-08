b3907eb8a838db0886c927501adfdd82a97a1249
isuhachev <lifeandfree@gmail.com>
2016-12-08 18:19:33 +0500

    fix

--- computing/src/main/java/ru/urfu/computing/Application.java
+++ computing/src/main/java/ru/urfu/computing/Application.java
@@ -10,13 +10,13 @@ public class Application {
     public static void main(String[] args) {
 
         System.out.println("Hello World!");
-
         Logfile.getInstance().getLogger().info(Application.class.getName() + ": Hello World!");
         Unhandled unhandled = new Unhandled("man4", "photo4", "tags4");
         DaoFactory.getInstance().getUnhandledDAO().addElement(unhandled);
         Collection<Unhandled> hh = DaoFactory.getInstance().getUnhandledDAO().getAllElements();
         System.out.println(hh);
         System.out.println(DaoFactory.getInstance().getUnhandledDAO().getElementByID(2L).toString());
+
     }
 
 }
