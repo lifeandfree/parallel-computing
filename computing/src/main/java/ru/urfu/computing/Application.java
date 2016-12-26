5b15b3954701f987d7006dcff48b9e6696b2676b
isuhachev <lifeandfree@gmail.com>
2016-12-27 03:25:08 +0500

    add download methods

--- computing/src/main/java/ru/urfu/computing/Application.java
+++ computing/src/main/java/ru/urfu/computing/Application.java
@@ -3,20 +3,26 @@ package ru.urfu.computing;
 import java.util.Collection;
 
 import ru.urfu.computing.server.core.dao.DaoFactory;
-import ru.urfu.computing.server.core.logger.Logfile;
 import ru.urfu.computing.server.core.model.unhandled.Unhandled;
+import ru.urfu.computing.server.download.flickr.Flickr;
 
 public class Application {
     public static void main(String[] args) {
 
-        System.out.println("Hello World!");
-        Logfile.getInstance().getLogger().info(Application.class.getName() + ": Hello World!");
-        Unhandled unhandled = DaoFactory.getInstance().getUnhandledDAO().addUnhandled("man4", "photo4", "tags4");
-        System.out.println(unhandled);
+        // System.out.println("Hello World!");
+        // Logfile.getInstance().getLogger().info(Application.class.getName() + ": Hello World!");
+        // Unhandled unhandled = DaoFactory.getInstance().getUnhandledDAO().addUnhandled("photo2", "tags3");
+        // System.out.println(unhandled);
 
+        // Collection<Unhandled> hh = DaoFactory.getInstance().getUnhandledDAO().getAllElements();
+        // System.out.println(hh);
+        long min_upload_date = 1482519900;
+        long max_upload_date = 1482521700;
+        long interval = 600;
+        Flickr.getAndWriteUnhanledToDB(min_upload_date, max_upload_date, interval);
         Collection<Unhandled> hh = DaoFactory.getInstance().getUnhandledDAO().getAllElements();
         System.out.println(hh);
-        System.out.println(DaoFactory.getInstance().getUnhandledDAO().getElementByID(2L).toString());
+        // System.out.println(DaoFactory.getInstance().getUnhandledDAO().getElementByID(2L).toString());
 
     }
 
--- computing/src/main/java/ru/urfu/computing/server/download/flickr/Flickr.java
+++ computing/src/main/java/ru/urfu/computing/server/download/flickr/Flickr.java
@@ -8,11 +8,115 @@
  */
 package ru.urfu.computing.server.download.flickr;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+
+import ru.urfu.computing.server.core.dao.DaoFactory;
+import ru.urfu.computing.server.core.logger.Logfile;
+import ru.urfu.computing.server.utils.IOUtilities;
+
 /**
  * @author lifeandfree
  */
 public class Flickr {
 
     public static final String API_KEY = "api_key";
+
+    /**
+     * @param apiKey
+     * @param sharedSecret
+     * @param parameters
+     * @param transport
+     */
+    public static void getAndWriteUnhanledToDBByPage(String apiKey, String sharedSecret, Map<String, Object> parameters,
+            Transport transport) {
+        try {
+            Response response = transport.get(transport.getPath(), parameters, apiKey, sharedSecret);
+            JSONArray photos = (JSONArray)response.getPhotos();
+            for (Object el : photos) {
+                JSONObject photo = (JSONObject)el;
+                String id = (String)photo.get("id");
+                DaoFactory.getInstance().getUnhandledDAO().addUnhandled(id, photo.toString());
+            }
+            response.setCurrentPage(response.getCurrentPage() + 1);
+            if ((response.getCurrentPage() <= response.getTotalPages())
+                    && (response.getCurrentPage() <= Response.MAX_PAGES)) {
+                parameters.put("page", response.getCurrentPage());
+                getAndWriteUnhanledToDBByPage(apiKey, sharedSecret, parameters, transport);
+            }
+        }
+        catch (FlickrException e) {
+            Logfile.getInstance().getLogger().error(e);
+        }
+    }
+
+    /**
+     * @param min_upload_date
+     * @param max_upload_date
+     */
+    public static void getAndWriteUnhanledToDBByTime(long min_upload_date, long max_upload_date) {
+        // params = "{per_page=10, method=flickr.photos.search, user_id=59159233@N04, format=json,
+        // extras=url_t,owner_name,machine_tags,date_upload,url_s,o_dims,media,url_sq,tags,geo,license,original_format,path_alias,last_update,url_o,icon_server,url_l,url_m,camera,date_taken,views,
+        // page=1}";
+    
+        Properties properties = null;
+        InputStream in = null;
+        String apiKey = null;
+        String sharedSecret = null;
+        try {
+            in = Test.class.getResourceAsStream("/auth.properties");
+            properties = new Properties();
+            properties.load(in);
+            apiKey = properties.getProperty("apiKey");
+            sharedSecret = properties.getProperty("sharedSecret");
+            properties.getProperty("user_id");
+        }
+        catch (IOException e) {
+            Logfile.getInstance().getLogger().error(e);
+        }
+        finally {
+            IOUtilities.close(in);
+        }
+    
+        Map<String, Object> parameters = new HashMap<String, Object>();
+        parameters.put("per_page", "250");
+        parameters.put("method", "flickr.photos.search");
+        parameters.put("format", "json");
+        parameters.put("has_geo", "1");
+        parameters.put("extras", "camera,geo");
+        parameters.put("page", "1");
+        parameters.put("media", "photo");
+        parameters.put("content_type", "1");
+        // parameters.put("extras",
+        // "url_t,owner_name,machine_tags,date_upload,url_s,o_dims,media,url_sq,tags,geo,license,original_format,path_alias,last_update,url_o,icon_server,url_l,url_m,camera,date_taken,views");
+        // java.sql.Timestamp timestamp = new java.sql.Timestamp(1482521700000L);
+    
+        parameters.put("min_upload_date", min_upload_date);
+    
+        parameters.put("max_upload_date", max_upload_date);
+    
+        Transport transport = new Rest();
+    
+        getAndWriteUnhanledToDBByPage(apiKey, sharedSecret, parameters, transport);
+    }
+
+    /**
+     * @param min_upload_date
+     * @param max_upload_date
+     * @param interval
+     * @return
+     */
+    public static void getAndWriteUnhanledToDB(long min_upload_date, long max_upload_date, long interval) {
+        while (min_upload_date < max_upload_date) {
+            getAndWriteUnhanledToDBByTime(min_upload_date, min_upload_date + interval);
+            min_upload_date += interval;
+        }
+    }
 
 }
