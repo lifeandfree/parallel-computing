7bc886bfde85c3f7010cc182c2172557080e2d2e
isuhachev <lifeandfree@gmail.com>
2016-12-27 03:24:36 +0500

    add Response

--- computing/src/main/java/ru/urfu/computing/server/download/flickr/RESTResponse.java
+++ computing/src/main/java/ru/urfu/computing/server/download/flickr/RESTResponse.java
@@ -8,40 +8,9 @@
  */
 package ru.urfu.computing.server.download.flickr;
 
-import org.w3c.dom.Document;
-
 /**
  * @author lifeandfree
  */
-public class RESTResponse implements Response {
-
-    private String errorCode;
-
-    private String errorMessage;
-
-    private String stat;
-
-    @Override
-    public String getErrorCode() {
-        return errorCode;
-    }
-
-    @Override
-    public String getErrorMessage() {
-        return errorMessage;
-    }
-
-    public String getStat() {
-        return stat;
-    }
-
-    @Override
-    public boolean isError() {
-        return errorCode != null;
-    }
-
-    @Override
-    public void parse(Document document) {
-    }
+public class RESTResponse extends Response {
 
 }
--- computing/src/main/java/ru/urfu/computing/server/download/flickr/Response.java
+++ computing/src/main/java/ru/urfu/computing/server/download/flickr/Response.java
@@ -8,18 +8,102 @@
  */
 package ru.urfu.computing.server.download.flickr;
 
-import org.w3c.dom.Document;
-
 /**
  * @author lifeandfree
  */
-public interface Response {
+public abstract class Response {
 
-    String getErrorCode();
+    private long currentPage;
+    private String errorCode;
+    private String errorMessage;
+    private Parser parser;
+    private Object photos;
+    private String stat;
+    private long totalPages;
+    public static final int MAX_PAGES = 16;
 
-    String getErrorMessage();
+    public long getCurrentPage() {
+        return currentPage;
+    }
 
-    boolean isError();
+    /**
+     * @return the errorCode
+     */
+    public String getErrorCode() {
+        return errorCode;
+    }
 
-    void parse(Document document);
+    /**
+     * @return the errorMessage
+     */
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    /**
+     * @return the parser
+     */
+    public Parser getParser() {
+        return parser;
+    }
+
+    public Object getPhotos() {
+        return photos;
+    }
+
+    /**
+     * @return the stat
+     */
+    public String getStat() {
+        return stat;
+    }
+
+    public long getTotalPages() {
+        return totalPages;
+    }
+
+    public void setCurrentPage(long currentPage) {
+        this.currentPage = currentPage;
+    }
+
+    /**
+     * @param errorCode
+     *            the errorCode to set
+     */
+    public void setErrorCode(String errorCode) {
+        this.errorCode = errorCode;
+    }
+
+    /**
+     * @param errorMessage
+     *            the errorMessage to set
+     */
+    public void setErrorMessage(String errorMessage) {
+        this.errorMessage = errorMessage;
+    }
+
+    /**
+     * @param parser
+     *            the parser to set
+     */
+    public void setParser(Parser parser) {
+        this.parser = parser;
+    }
+
+    public void setPhotos(Object photos) {
+        this.photos = photos;
+    }
+
+    /**
+     * @param stat
+     *            the stat to set
+     */
+    public void setStat(String stat) {
+        this.stat = stat;
+    }
+
+    public void setTotalPages(long totalPages) {
+        this.totalPages = totalPages;
+    }
+
 }
