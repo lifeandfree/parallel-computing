39afd84828e760000a67bb398d62f1e11cffd164
isuhachev <lifeandfree@gmail.com>
2017-01-19 18:13:37 +0500

    add tarcam

---
+++ tarcam/src/main/java/ru/urfu/computing/tarcam/site/servlet/MainPage.java
@@ -0,0 +1,42 @@
+/**
+ * Организация: УрФУ
+ * Проект: Параллельные вычисления, 2016
+ *
+ * @author lifeandfree
+ *         Пакет: ru.urfu.computing.tarcam.site.servlet
+ *         Дата создания класса: 19 янв. 2017 г.
+ */
+package ru.urfu.computing.tarcam.site.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author lifeandfree
+ */
+public class MainPage extends HttpServlet {
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = -1924984265408926223L;
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        req.setAttribute("title", "tarcam");
+        req.setAttribute("body", "tarcam");
+        req.getRequestDispatcher("main_page_template.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        super.doPost(req, resp);
+    }
+
+}
---
+++ tarcam/src/main/webapp/main_page_template.jsp
@@ -0,0 +1,15 @@
+<%@ page contentType="text/html;charset=UTF-8" language="java" %><!DOCTYPE html>
+<html lang="RU">
+<head>
+    <meta charset="utf-8" />
+    <title>${title}</title>
+    <meta name="keywords" content="${keywords}" />
+	<meta name="description" content="${description}" />    
+    ${styles}
+</head>
+<body>
+		${HEADER}
+		${body}
+		${footer}
+</body>
+</html>
