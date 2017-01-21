64321601193ba7014cdb3ae8380d3a386b742a20
isuhachev <lifeandfree@gmail.com>
2017-01-21 23:00:37 +0500

    create ConnectionFactory

---
+++ computing/src/main/java/ru/urfu/computing/server/core/db/jdbc/ConnectionFactory.java
@@ -0,0 +1,61 @@
+/**
+ * Организация: УрФУ
+ * Проект: Параллельные вычисления, 2016
+ *
+ * @author lifeandfree
+ *         Пакет: ru.urfu.computing.server.core.db
+ *         Дата создания класса: 18 янв. 2017 г.
+ */
+package ru.urfu.computing.server.core.db.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import ru.urfu.computing.server.core.logger.Logfile;
+
+/**
+ * @author lifeandfree
+ *         Фабрика по созданию соединений для подключения к БД.
+ */
+public class ConnectionFactory {
+    // public static final String DRIVER_CLASS_MYSQL = "com.mysql.jdbc.Driver";
+    private static String DRIVER_CLASS_HIVE = "org.apache.hive.jdbc.HiveDriver";
+
+    private static ConnectionFactory instance = new ConnectionFactory(DRIVER_CLASS_HIVE);
+
+    public static Connection getConnection(String url, String user, String password) {
+        return instance.createConnection(url, user, password);
+    }
+
+    private ConnectionFactory(String driverClass) {
+        try {
+            Class.forName(driverClass);
+        }
+        catch (ClassNotFoundException e) {
+            Logfile.getInstance().getLogger().error("Unable to load " + driverClass, e);
+        }
+        catch (Exception e) {
+            Logfile.getInstance().getLogger().error("Unable to load " + driverClass + " Message: " + e.getMessage(), e);
+        }
+    }
+
+    private Connection createConnection(String url, String user, String password) {
+        Connection connection = null;
+        try {
+            connection = DriverManager.getConnection(url, user, password);
+            DbUtil.checkWarnings(connection.getWarnings());
+            Logfile.getInstance().getLogger().info("Connect to Database.");
+        }
+        catch (SQLException e) {
+            Logfile.getInstance().getLogger().error("Unable to Connect to Database." + " SQLState: " + e.getSQLState()
+                    + " Message: " + e.getMessage() + " Vendor code: " + e.getErrorCode(), e);
+        }
+        catch (Exception e) {
+            Logfile.getInstance().getLogger().error("Unable to Connect to Database." + " Message: " + e.getMessage(),
+                    e);
+        }
+        return connection;
+    }
+
+}
