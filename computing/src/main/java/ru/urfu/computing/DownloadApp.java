adbc1f2b263129469d830930d798a156f42a11f1
isuhachev <lifeandfree@gmail.com>
2017-01-22 00:50:39 +0500

    add DownloadApp

---
+++ computing/src/main/java/ru/urfu/computing/DownloadApp.java
@@ -0,0 +1,33 @@
+/**
+ * Организация: УрФУ
+ * Проект: Параллельные вычисления, 2016
+ *
+ * @author lifeandfree
+ *         Пакет: ru.urfu.computing
+ *         Дата создания класса: 21 янв. 2017 г.
+ */
+package ru.urfu.computing;
+
+import ru.urfu.computing.server.download.flickr.Flickr;
+
+/**
+ * @author lifeandfree
+ */
+public class DownloadApp {
+
+    public static void main(String[] args) {
+        long min_upload_date = 1482519900;
+        long max_upload_date = 1482521700;
+        // long max_upload_date = 1483819900;
+        long interval = 600;
+        Flickr flickr = new Flickr();
+        flickr.getAndWriteUnhanledToDB(min_upload_date, max_upload_date, interval);
+    }
+
+    /**
+     *
+     */
+    public DownloadApp() {
+    }
+
+}
